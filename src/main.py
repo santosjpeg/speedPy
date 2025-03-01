19c9bb22a50db6ab0ede46d048c51c0c18bca4ff
Aaron <santosaaron2004@gmail.com>
2025-02-28 22:22:09 -0500

    complete game setup

--- src/main.py
+++ src/main.py
@@ -7,22 +7,77 @@ import pycardengine.pycard as pycard
 
 class Speed:
     def __init__(self):
-        #GAME SETUP
-        #   1. INIT TABLE
-        #   2. ADD 'test' DECK
-        #   3. CREATE CARDS IN TEST DECK ()
         table = pycard.Table()
-        table.add_deck('test',
-                       (500,500),
-                       (0,0),
+
+        #INITIALIZE ALL 4 DECKS
+
+        #CENTER LEFT AND CENTER RIGHT DESKS FOR FLIPPING CARDS BEFORE EACH ROUND; 6 CARDS EACH
+        table.add_deck('center_deck_1',
+                       (WINDOW_WIDTH // 4, WINDOW_HEIGHT // 2),
+                       (0,-1),
                        False)
         
-        table.create_cards_in_deck('test', (1,13),(1,13),(1,13),(1,13))
+        table.create_cards_in_deck('center_deck_1', (1,13),(1,13),(1,13),(1,13))
+        table['center_deck_1'].shuffle()
 
+        table.add_deck('center_deck_2',
+                       (WINDOW_WIDTH // (4/3), WINDOW_HEIGHT // 2),
+                       (0,-1),
+                       False)
+        for i in range(6):
+            table.deal_to_top('center_deck_1', 'center_deck_2')
+        table['center_deck_2'].shuffle()
+
+        #USER AND OPPONENT DECK; 20 CARDS EACH
+        table.add_deck('user_deck',
+                       (WINDOW_WIDTH // 8, WINDOW_HEIGHT // (4/3)),
+                       (0,-1),
+                       False)
+        for i in range(20):
+            table.deal_to_top('center_deck_1', 'user_deck')
+        table['user_deck'].shuffle()
+
+        table.add_deck('opponent_deck',
+                       (WINDOW_WIDTH // (8/7), WINDOW_HEIGHT // 4),
+                       (0,-1),
+                       False)
+        for i in range(20):
+            table.deal_to_top('center_deck_1','opponent_deck')
+        table['opponent_deck'].shuffle()
+
+        #INITIALIZE USER + OPPONENT HAND
+        table.add_deck('user_hand',
+                       (WINDOW_WIDTH // 4, WINDOW_HEIGHT // (4/3)),
+                       (40,0),
+                       True)
+        for i in range(5):
+            table.deal_to_top('user_deck', 'user_hand')
         
-        
-        table['test'].shuffle()
+        table.add_deck('opponent_hand',
+                       (WINDOW_WIDTH // (8/6), WINDOW_HEIGHT // 4),
+                       (-40,0),
+                       True)
+        for i in range(5):
+            table.deal_to_top('opponent_deck','opponent_hand')
+
+        #chaotic but bear with me...
+        table.add_deck('center_flipped_1',
+                       (WINDOW_WIDTH_HALF + 100,WINDOW_HEIGHT // 2),
+                       (1,0),
+                       True)
+        table.deal_to_top('center_deck_1', 'center_flipped_1')
+
+        table.add_deck('center_flipped_2',
+                       (WINDOW_WIDTH_HALF - 100,WINDOW_HEIGHT // 2),
+                       (1,0),
+                       True)
+        table.deal_to_top('center_deck_2', 'center_flipped_2')
         self.table = table
+    
+    def interact(self, mouse_x_y : tuple[int , int]) -> None:
+        target = self.table.click_deck(mouse_x_y)
+        if target:
+            name , d , c = target 
     
     def draw(self, display_surface : pygame.Surface, off : float = 0) -> None:
         self.table.draw(display_surface)
@@ -61,11 +116,17 @@ def main() -> None:
 
         display_surface.fill(POKER_GREEN)
         
+        mxy = mx , my = pygame.mouse.get_pos()
         #EVENT HANDLER
         for event in pygame.event.get():
             match event.type:
                 case pygame.QUIT:
                     handle_quit()
+                    
+                case pygame.MOUSEBUTTONDOWN:
+                    match event.dict['button']:
+                        case 1:
+                            speed.interact(mxy)
                     
                 
         speed.draw(display_surface, offset)
--- test.py
+++ test.py
@@ -1,5 +1,18 @@
+import pygame 
+import thorpy as tp 
 
-xy = (1,2)
-x, y = xy
+pygame.init()
 
-print(x, y)
+screen = pygame.display.set_mode((1920,1080))
+tp.init(screen, tp.theme_human)
+
+my_button = tp.Button("Hello World.\nThis button uses the default theme")
+my_button.center_on(screen)
+
+def before_gui():
+    screen.fill((250,)*3)
+
+tp.call_before_gui(before_gui)
+
+player = my_button.get_updater().launch()
+pygame.quit()
